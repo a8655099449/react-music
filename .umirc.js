ee764f6c53ed50159e34f7c6ee0e1609f5240b89
大步佬 <865509949@qq.com>
2021-09-27 22:31:42 +0800

    2021年9月27日22:31:41

--- .umirc.js
+++ .umirc.js
@@ -27,5 +27,9 @@ export default defineConfig({
     { component: '@/pages/404' },
   ],
   outputPath: '../static-music2',
+  history: {
+    type: 'browser',
+  },
+
   // publicPath:'/src/assets/'
 });
